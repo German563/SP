53570de5c2c6c5628e1f4cf8365952ee967c9080
German563 <73666252+German563@users.noreply.github.com>
2023-04-02 20:36:56 +0300

    new commit

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const languageButton = document.querySelectorAll(".language");
-
-
+const languageButtons = document.querySelectorAll('.language');
 
 const moveElement = () => {
-    const hebrewPage = document.querySelector('.he-page');
-    const russianPage = document.querySelector(".ru-page");
-  
-    if (hebrewPage.style.display === 'block') {
-      hebrewPage.style.display = 'none';
-      russianPage.style.display = 'block';
-    } else {
-      hebrewPage.style.display = 'block';
-      russianPage.style.display = 'none';
-    }
-  };
-  for (let i = 0; i < languageButton.length; i++) {
-    languageButton[i].addEventListener('click', moveElement);
+  const hebrewPage = document.querySelector('.he-page');
+  const russianPage = document.querySelector('.ru-page');
+
+  if (hebrewPage.style.display === 'block') {
+    hebrewPage.style.display = 'none';
+    russianPage.style.display = 'block';
+  } else {
+    hebrewPage.style.display = 'block';
+    russianPage.style.display = 'none';
   }
+};
 
-  const phoneNumber = document.querySelector('.main__phone__number');
- 
-let i = 0;
+languageButtons.forEach(button => {
+  button.addEventListener('click', moveElement);
+});
 
-// let isBlue = false;
+const phoneNumbers = document.querySelectorAll('.main__phone__number');
 
-// setInterval(() => {
-//   const textArray = phoneNumber.textContent.split('');
-//   phoneNumber.innerHTML = textArray
-//     .map((char, i) => `<span class="${(i % 2 === 1) ? (isBlue ? 'blue' : '') : ''}">${char}</span>`)
-//     .join('');
-//   isBlue = !isBlue;
-// }, 1000);
+phoneNumbers.forEach(phoneNumber => {
+  let isBlue = false;
+
+  setInterval(() => {
+    const textArray = phoneNumber.textContent.split('');
+    phoneNumber.innerHTML = textArray
+      .map((char, i) => `<span class="${(i % 2 === 0 && isBlue) ? 'blue' : ''}">${char}</span>`)
+      .join('');
+    isBlue = !isBlue;
+  }, 1000);
+});
